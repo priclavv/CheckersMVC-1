fe4174bd14d252f162996e2aa3fde790d0dfee60
Kacper Drejer <kacper.drejer@gmail.com>
2017-05-27 22:21:54 +0200

    add missing view and controller

---
+++ CheckersMVC/Controllers/LeaderboardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CheckersMVC.Models;
+
+namespace CheckersMVC.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        // GET: Leaderboard
+        public ActionResult Index()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var playerStatsList = db.PlayerStatsList.ToList();
+            playerStatsList.Sort((x, y) => x.GamesWonCount > y.GamesWonCount ? 1 : -1);
+            return View(playerStatsList);
+        }
+
+        // GET: Leaderboard/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
